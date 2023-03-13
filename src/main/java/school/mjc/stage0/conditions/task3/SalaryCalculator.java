aa75c74dc06c5488fe88592f9d773351af96209c
macservis <fboqiyev1065@gmail.com>
2023-03-13 12:53:10 +0500

    stage0-module4-conditions-task3

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,22 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        double taxes;
+        double salareis;
+        if ( salary > 0 && salary <= 10000){
+            taxes = 0.15;
+            salareis = salary - salary * taxes;
+            System.out.println(salareis);
+        } else if (salary > 10000 && salary <= 20000) {
+            taxes = 0.18;
+            salareis = salary - salary * taxes;
+            System.out.println(salareis);
+        } else if (salary > 20000) {
+            taxes = 0.20;
+            salareis = salary - salary * taxes;
+            System.out.println(salareis);
+        }else if(salary < 0){
+            System.out.println("wrong input!");
+        }
     }
 }
